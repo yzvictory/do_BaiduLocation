296c91e829f7fdc941552db4e4ddd7e29a5c91de
maoruiily <maoruiily@163.com>
2015-04-09 14:09:29 +0800

    Android 添加 百度定位 功能

---
+++ src/android/doExt_do_BaiduLocation/src/extdefine/do_BaiduLocation_IMethod.java
@@ -0,0 +1,14 @@
+package extdefine;
+
+import core.helper.jsonparse.DoJsonNode;
+import core.interfaces.DoIScriptEngine;
+import core.object.DoInvokeResult;
+
+/**
+ * 声明自定义扩展组件方法
+ */
+public interface do_BaiduLocation_IMethod {
+	void getLocation(DoJsonNode _dictParas,DoIScriptEngine _scriptEngine, String _callbackFuncName) throws Exception ;
+
+	void stop(DoJsonNode _dictParas, DoIScriptEngine _scriptEngine, DoInvokeResult _invokeResult);
+}
